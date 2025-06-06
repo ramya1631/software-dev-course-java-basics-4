55c4468526f0e5a2de25c2b403c7f0fad13bf08b
ramya1631 <ramya.koluguri16@gmail.com>
2025-06-06 00:48:45 -0500

    finished collection exercises,student manager

--- src/main/java/org/example/CollectionChoiceExercises.java
+++ src/main/java/org/example/CollectionChoiceExercises.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 // For these exercises, you need to declare a single variable
 // in each function that uses the appropriate collection type
 // (Array, ArrayList, HashSet, HashMap)
-//
 // You do not need to populate this array or add any additional code.
 // If you get stuck, refer to the java basics 4 (arrays and collections) curriculum
 public class CollectionChoiceExercises {
@@ -13,12 +16,13 @@ public class CollectionChoiceExercises {
         // of three strings that are known ahead of time to be "Red", "Green" and "Blue"
         // and will not change during program execution.
 
-        String[] colors;
+        String[] colors = {"Red", "Green", "Blue"};
     }
 
     public void MakeStudentCollection() {
         // Create an appropriate collection variable for holding a list of student names
         // that will be added to and removed from during program execution.
+        ArrayList<String> studentNames = new ArrayList<>();
     }
 
     public void MakeLoggedInUsersCollection() {
@@ -26,11 +30,14 @@ public class CollectionChoiceExercises {
         // unique logged in usernames where adding a username that's already
         // present is ignored and checking if a user is in the collection can
         // be done very quickly.
+        HashSet<String> loggedInUsers = new HashSet<>();
+
     }
 
     public void MakeEmployeeAccessCodesCollection() {
         // Create an appropriate collection variable for holding a set of
         // key-value pairs where they key is an employee name (String) and the
         // value is an employee access code (String.)
+        HashMap<String, String> employeeAccessCodes = new HashMap<>();
     }
 }
--- src/main/java/org/example/StudentManager.java
+++ src/main/java/org/example/StudentManager.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class StudentManager {
     private Scanner scanner = new Scanner(System.in);
     private List<String> students = new ArrayList<>();
@@ -35,7 +36,10 @@ public class StudentManager {
         // Prompt the user to enter a new student name (using scanner and I/O methods learned previously,
         // refer to mainMenu() for an example)
         // Add the student to the list
-
+        System.out.print("Enter student name to add: ");
+        String name = scanner.nextLine();
+        students.add(name);
+        System.out.println(name + " has been added.");
         mainMenu();
     }
 
@@ -43,13 +47,30 @@ public class StudentManager {
         // Prompt the user for a student name
         // Use the contains method to check if the student entered is in the list
         // If so, remove it, if not, print "Student not found."
-
+        System.out.print("Enter student name to remove: ");
+        String name = scanner.nextLine();
+        if (students.contains(name)) {
+            students.remove(name);
+            System.out.println(name + " has been removed.");
+        } else {
+            System.out.println("Student not found.");
+        }
         mainMenu();
     }
 
     public void viewStudents() {
         // Loop through the list of students and print each one
         // (Use a for-each loop!)
+
+         if (students.isEmpty()) {
+            System.out.println("No students to display.");
+        } else {
+            System.out.println("Current students:");
+            for (String student : students) {
+                System.out.println("- " + student);
+            }
+        }
         mainMenu();
     }
 }
+
